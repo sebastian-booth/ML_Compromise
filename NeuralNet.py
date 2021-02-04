384b516926446f74f1931f1101e3e381a8f1e71b
sbooth <csc@localhost>
2021-02-04 02:27:03 +0000

    Tidied code and added comments to all scripts, created separate train player file. Added trained player and best-player pickle

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,60 +1,58 @@
 import numpy as np
-## neural network
 
-def relu(input_layer):
-    return np.maximum(0, input_layer)
+def relu(input):
+    # Return the rectified linear for the hidden layer
+    return np.maximum(0, input)
 
-def softmax(input_layer):
-    calc_exp = np.exp(input_layer - np.max(input_layer, axis=0, keepdims=True)) #axis = 1 ?
-    return calc_exp / np.sum(calc_exp, axis=0, keepdims=True) # axis = 1 ?
+def softmax(input):
+    # Return the softmax probabilities for the output layer
+    calc_exp = np.exp(input - np.max(input, axis=0, keepdims=True))
+    return calc_exp / np.sum(calc_exp, axis=0, keepdims=True)
 
 class NeuralNetwork:
     def __init__(self):
+        # Randomly generate bias between 1 and 3 and layer weights between 0 and 1 in format [54, 45, 45, 27]
+        # load desired output
         self.bias = np.random.randint(1,3)
-        self.weights_input_to_hidden = np.random.rand(54,45)
-        self.weights_hidden_to_output = np.random.rand(45,27)
+        self.weights_hidden = np.random.rand(54, 45)
+        self.weights_output = np.random.rand(45, 27)
         self.desired_output_joined = np.array([111, 112, 113, 121, 122, 123, 131, 132, 133,
                                        211, 212, 213, 221, 222, 223, 231, 232, 233,
                                        311, 312, 313, 321, 322, 323, 331, 332, 333])
-        self.desired_output = [[1, 1, 1], [1, 1, 2], [1, 1, 3], [1, 2, 1], [1, 2, 2], [1, 2, 3], [1, 3, 1],
-                               [1, 3, 2], [1, 3, 3], [2, 1, 1], [2, 1, 2], [2, 1, 3], [2, 2, 1], [2, 2, 2],
-                               [2, 2, 3], [2, 3, 1], [2, 3, 2], [2, 3, 3], [3, 1, 1], [3, 1, 2], [3, 1, 3],
-                               [3, 2, 1], [3, 2, 2], [3, 2, 3], [3, 3, 1], [3, 3, 2], [3, 3, 3]]
 
     def feedforward(self, input_layer):
-        ## relu for input to hidden
-        self.layer_input_to_hidden = relu(np.dot(input_layer, self.weights_input_to_hidden) + self.bias)
-        ## softmax for hidden to output
-        #print(self.layer_input_to_hidden)
-        #print(len(self.layer_input_to_hidden))
-        self.layer_hidden_to_output = softmax(np.dot(self.layer_input_to_hidden, self.weights_hidden_to_output) + self.bias)
-        #print(self.layer_hidden_to_output)
-        #print(len(self.layer_hidden_to_output))
-        return self.layer_hidden_to_output
+        # Load input layer and hidden layer weights as a dot product, adding bias and return relu result
+        # Load values returned from relu  and the output layer weights as a dot product, adding bias and return softmax result
+        # Return softmax probabilities
+        self.layer_hidden = relu(np.dot(input_layer, self.weights_hidden) + self.bias)
+        self.layer_output = softmax(np.dot(self.layer_hidden, self.weights_output) + self.bias)
+        return self.layer_output
 
-    def train(self, input_layer, myScore, oppScore):
+    def get_move(self, input_layer):
+        # Run feedforward method, get the index of the maximum output layer weight, get the value of this index and map
+        # onto desired output array, reformat the value as an int list and minus 1 from each element to make it a valid game input
         feedforward_res = self.feedforward(input_layer)
-        get_max_weight = np.argmax(feedforward_res,axis=0) ## get index of maximum output neron weight
-        get_max_weight_val = int(self.desired_output_joined[[get_max_weight]]) ## get value from index above
-        res_clear_move = [int(z) for z in str(get_max_weight_val)] ## Format get_max_weight_val as int list valid for game input
-        res = [v-1 for v in res_clear_move] ## Minus 1 from move result for valid input
-        #self.ga.fitness(feedforward_res, myScore, oppScore, res)
+        get_max_weight = np.argmax(feedforward_res,axis=0)
+        get_max_weight_val = int(self.desired_output_joined[[get_max_weight]])
+        res_clear_move = [int(z) for z in str(get_max_weight_val)]
+        res = [v-1 for v in res_clear_move]
         return res
 
-    def get_weights_input_to_hidden(self):
-        return self.weights_input_to_hidden
+    # get and set self variable functions
+    def get_weights_hidden(self):
+        return self.weights_hidden
 
-    def get_weights_hidden_to_output(self):
-        return self.weights_hidden_to_output
+    def get_weights_output(self):
+        return self.weights_output
 
     def get_bias(self):
         return self.bias
 
-    def set_weights_input_to_hidden(self, set_val):
-        self.weights_input_to_hidden = set_val
+    def set_weights_hidden(self, set_val):
+        self.weights_hidden = set_val
 
-    def set_weights_hidden_to_output(self, set_val):
-        self.weights_hidden_to_output = set_val
+    def set_weights_output(self, set_val):
+        self.weights_output = set_val
 
     def set_bias(self, set_val):
         self.bias = set_val
